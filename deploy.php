336927fc6508f55e25fab115468d104fa2ba99cf
unknown <viktor.avelino@gmail.com>
2021-02-27 16:29:51 -0300

    teste

--- deploy.php
+++ deploy.php
@@ -23,15 +23,48 @@ EOT;
 
 // Check whether client is allowed to trigger an update
 
+$allowed_ips = array(
+	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
+	'195.37.139.','193.174.' // VZG
+);
+$allowed = true;
+
+$headers = apache_request_headers();
+
+if (@$headers["X-Forwarded-For"]) {
+    $ips = explode(",",$headers["X-Forwarded-For"]);
+    $ip  = $ips[0];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+foreach ($allowed_ips as $allow) {
+    if (stripos($ip, $allow) !== true) {
+        $allowed = true;
+        break;
+    }
+}
+
+//if (!$allowed) {
+//	header('HTTP/1.1 403 Forbidden');
+ //	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
+ //   echo "</pre>\n</body>\n</html>";
+//    exit;
+//}
+
+flush();
+
+// Actually run the update
+
 $commands = array(
 	'echo $PWD',
-	//'whoami',
+	'whoami',
 	'git pull origin master',
 	'git status',
 	'git submodule sync',
 	'git submodule update',
 	'git submodule status',
-   //'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
+    'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
 );
 
 $output = "\n";
@@ -40,7 +73,7 @@ $log = "####### ".date('Y-m-d H:i:s'). " #######\n";
 
 foreach($commands AS $command){
     // Run it
-    $tmp = shell_exec("$command");// 2>&1");
+    $tmp = shell_exec("$command 2>&1");
     // Output
     $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
     $output .= htmlentities(trim($tmp)) . "\n";
